a2db30f58c2b14e8f75f8de3b35fd53b1b89722c
Huang Yaming <yummin@ccs.neu.edu>
2015-11-28 20:09:55 -0500

    Fix unit test

--- src/test/java/wham/QueryTest.java
+++ src/test/java/wham/QueryTest.java
@@ -2,6 +2,11 @@ package wham;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import wham.model.User;
@@ -9,9 +14,17 @@ import wham.operation.UserOperation;
 
 public class QueryTest {
 
+    private EntityManager em;
+
+    @Before
+    public void setUp() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("WHAM");
+        this.em = emf.createEntityManager();
+    }
+
     @Test
     public void testGetUserByEmail() {
-        UserOperation operation = new UserOperation();
+        UserOperation operation = new UserOperation(em);
         User u = operation.getUser("abc@edf.com", "password");
         assertEquals(2, u.getUId());
     }
