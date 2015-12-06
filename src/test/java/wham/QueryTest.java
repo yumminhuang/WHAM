ae7ca0a23d36fec2f254e368bfbefdea21e740eb
Huang Yaming <yummin@ccs.neu.edu>
2015-12-06 10:32:53 -0500

    Adding more JUnit test cases

--- src/test/java/wham/QueryTest.java
+++ src/test/java/wham/QueryTest.java
@@ -1,18 +1,20 @@
 package wham;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
-import junit.framework.TestCase;
-import wham.config.AppEntityManager;
+import wham.model.Event;
 import wham.model.User;
+import wham.operation.BookingOperation;
+import wham.operation.EventOperation;
+import wham.operation.PreferenceOperation;
 import wham.operation.UserOperation;
 
-public class QueryTest extends TestCase {
-
-    @Override
-    public void setUp() {
-        AppEntityManager.initEntityManagerFactory();
-    }
+public class QueryTest extends JPATestCase {
 
     @Test
     public void testGetUserByEmail() {
@@ -21,9 +23,41 @@ public class QueryTest extends TestCase {
         assertEquals(2, u.getUId());
     }
 
-    @Override
-    public void tearDown() {
-        AppEntityManager.closeEntityManagerFactory();
+    @Test
+    public void testCheckingEventExist() {
+        EventOperation operation = new EventOperation();
+        assertEquals(true, operation.eventExist("17546444889"));
+    }
+
+    @Test
+    public void testQueryPreferences() {
+        PreferenceOperation o = new PreferenceOperation();
+        assertEquals("[3011, 10001, 16006]", o.getSubCategory("abc@edf.com").toString());
+    }
+
+    @Test
+    public void testQueryBookingReviews() {
+        BookingOperation bo = new BookingOperation();
+        List<String> reviews = bo.getAllReviewsForEvent("17546444889");
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("good");
+        assertEquals(list, reviews);
+    }
+
+    @Test
+    public void testQueryBookingRates() {
+        BookingOperation bo = new BookingOperation();
+        assertEquals(4.0, bo.averageRate("17546444889"), 0.01);
+    }
+
+    @Test
+    public void testQueryBookingHistory() {
+        BookingOperation bo = new BookingOperation();
+        List<String> list = new ArrayList<String>();
+        for (Event e : bo.getAllBookingByUser("abc@edf.com"))
+            list.add(e.getEId());
+        assertEquals("17546444889", list.get(0));
+        assertEquals("18941183590", list.get(1));
     }
 
 }
