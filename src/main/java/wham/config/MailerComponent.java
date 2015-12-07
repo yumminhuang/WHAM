b4eff50fb0543c4176ab4a5078e16f228fae2723
Huang Yaming <yummin@ccs.neu.edu>
2015-12-07 10:04:00 -0500

    Fixing Gmail AuthenticationFailedException by using SSL connection

--- src/main/java/wham/config/MailerComponent.java
+++ src/main/java/wham/config/MailerComponent.java
@@ -20,10 +20,11 @@ public class MailerComponent {
         final String password = "nu@12345";
 
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
 
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
